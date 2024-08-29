48d09b8fec8be2d759ffbab09b71e7add2ede2c1
Bourbxn <jakkapat97@gmail.com>
2024-08-29 13:44:29 +0700

    ✨ [APP-0048] Create PUT /api/common/user_info

--- server/handlers/common_handler.go
+++ server/handlers/common_handler.go
@@ -4,11 +4,14 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"golang.org/x/crypto/bcrypt"
 
+	"github.com/Project-IPCA/ipca-backend/pkg/requests"
 	"github.com/Project-IPCA/ipca-backend/pkg/responses"
 	"github.com/Project-IPCA/ipca-backend/pkg/utils"
 	"github.com/Project-IPCA/ipca-backend/repositories"
 	s "github.com/Project-IPCA/ipca-backend/server"
+	userservice "github.com/Project-IPCA/ipca-backend/services/user"
 )
 
 type CommonHandler struct {
@@ -30,6 +33,61 @@ func NewCommonHandler(server *s.Server) *CommonHandler {
 func (commonHandler *CommonHandler) GetUserInfo(c echo.Context) error {
 	userRepository := repositories.NewUserRepository(commonHandler.server.DB)
 	existUser := utils.GetUserClaims(c, *userRepository)
+	response := responses.NewUserInfoResponse(existUser)
+	return responses.Response(c, http.StatusOK, response)
+}
+
+// @Description Update User Info
+// @ID supervisor-update-user-info
+// @Tags Common
+// @Accept json
+// @Produce json
+// @Param params body	requests.UpdateUserInfoRequest	true	"User Info Request"
+// @Success 200		{object}	responses.UserInfoResponse
+// @Failure 400		{object}	responses.Error
+// @Security BearerAuth
+// @Router			/api/common/user_info [put]
+func (commonHandler *CommonHandler) UpdateUserInfo(c echo.Context) error {
+	updateUserInfoReq := new(requests.UpdateUserInfoRequest)
+
+	userRepository := repositories.NewUserRepository(commonHandler.server.DB)
+	existUser := utils.GetUserClaims(c, *userRepository)
+
+	if err := c.Bind(updateUserInfoReq); err != nil {
+		return responses.ErrorResponse(c, http.StatusBadRequest, "Invalid Required Field")
+	}
+
+	if err := updateUserInfoReq.BasicUserInfo.Validate(); err != nil {
+		return responses.ErrorResponse(
+			c,
+			http.StatusBadRequest,
+			"Invalid Request",
+		)
+	}
+
+	if updateUserInfoReq.NewPassword != nil && updateUserInfoReq.ConfirmNewPassword != nil &&
+		(*updateUserInfoReq.NewPassword != *updateUserInfoReq.ConfirmNewPassword) {
+		return responses.ErrorResponse(
+			c,
+			http.StatusBadRequest,
+			"Invalid Request",
+		)
+	}
+
+	if bcrypt.CompareHashAndPassword(
+		[]byte(existUser.Password),
+		[]byte(updateUserInfoReq.CurrentPassword),
+	) != nil {
+		return responses.ErrorResponse(
+			c,
+			http.StatusBadRequest,
+			"Password is not correct.",
+		)
+	}
+
+	userService := userservice.NewUserService(commonHandler.server.DB)
+	userService.UpdateUserInfo(&existUser, updateUserInfoReq)
+
 	response := responses.NewUserInfoResponse(existUser)
 	return responses.Response(c, http.StatusOK, response)
 }
--- services/user/update.go
+++ services/user/update.go
@@ -3,20 +3,67 @@ package user
 import (
 	"time"
 
+	"golang.org/x/crypto/bcrypt"
+
 	"github.com/Project-IPCA/ipca-backend/models"
+	"github.com/Project-IPCA/ipca-backend/pkg/requests"
 )
 
-func (userService *Service) UpdateLoginSuccess(user *models.User) error {
+func (userService *Service) UpdateLoginSuccess(user *models.User) {
 	timeNow := time.Now()
 	user.LastLogin = &timeNow
 	user.LastSeen = timeNow
 	user.IsOnline = true
 	userService.DB.Save(user)
-	return nil
 }
 
-func (userService *Service) UpdateIsOnline(user *models.User, isOnline bool) error {
+func (userService *Service) UpdateIsOnline(user *models.User, isOnline bool) {
 	user.IsOnline = isOnline
 	userService.DB.Save(user)
-	return nil
+}
+
+func (userService *Service) UpdateUserInfo(
+	user *models.User,
+	request *requests.UpdateUserInfoRequest,
+) {
+	var dob *time.Time
+	if request.DOB != nil {
+		dobParse, err := time.Parse("2006-01-02", *request.DOB)
+		if err == nil {
+			dob = &dobParse
+		}
+		user.DOB = dob
+	}
+
+	if request.Avatar != nil {
+		user.Avatar = request.Avatar
+	}
+
+	if request.Nickname != nil {
+		user.Nickname = request.Nickname
+	}
+
+	if request.Gender != nil {
+		user.Gender = request.Gender
+	}
+
+	if request.Email != nil {
+		user.Email = request.Email
+	}
+
+	if request.Tel != nil {
+		user.Tel = request.Tel
+	}
+
+	if request.NewPassword != nil {
+		encryptedPassword, err := bcrypt.GenerateFromPassword(
+			[]byte(*request.NewPassword),
+			bcrypt.DefaultCost,
+		)
+		if err == nil {
+			user.Password = string(encryptedPassword)
+		}
+	}
+
+	userService.DB.Save(user)
 }
