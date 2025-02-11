8dd27762026a7f4e0ae0d60578f141cadb4c16c9
bourbxn <jakkapat97@gmail.com>
2025-02-11 17:03:23 +0700

    🐛 [APP-0162] Fix mysql migration script

--- db/migrations/000016_update_department_table_add_name_en.down.sql
+++ db/migrations/000016_update_department_table_add_name_en.down.sql
@@ -1,17 +1,3 @@
-SET @dbname = DATABASE();
-SET @tablename = "departments";
-SET @columnname = "name_en";
-SET @preparedStatement = (SELECT IF(
-  (
-    SELECT COUNT(*) FROM INFORMATION_SCHEMA.COLUMNS
-    WHERE
-      (table_name = @tablename)
-      AND (table_schema = @dbname)
-      AND (column_name = @columnname)
-  ) < 1,
-  "SELECT 1",
-  CONCAT("ALTER TABLE ", @tablename, " DROP ", @columnname)
-));
-PREPARE alterIfNotExists FROM @preparedStatement;
-EXECUTE alterIfNotExists;
-DEALLOCATE PREPARE alterIfNotExists;
+ALTER TABLE `departments`
+DROP COLUMN `name_en`;
+
