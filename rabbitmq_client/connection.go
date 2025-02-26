87705b84b98311cf37682fd46a8744356c444a50
ootsirasit <ootsirasit@gmail.com>
2025-02-26 16:42:32 +0700

    🚑️ [HOTFIX-0167] Fix rabbitmq channel id space exhausted

--- rabbitmq_client/connection.go
+++ rabbitmq_client/connection.go
@@ -6,14 +6,28 @@ import (
 	"github.com/Project-IPCA/ipca-backend/config"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
-  
-func RabbitMQClient(cfg *config.Config) *amqp.Connection{
+
+type RabbitMQConnection struct {
+	Conn *amqp.Connection
+	Ch   *amqp.Channel
+}
+
+func RabbitMQClient(cfg *config.Config) *RabbitMQConnection {
 	url := fmt.Sprintf("amqp://%s:%s@%s:%s/", cfg.RabbitMQ.User, cfg.RabbitMQ.Password, cfg.RabbitMQ.Host, cfg.RabbitMQ.Port)
-	
+
 	conn, err := amqp.Dial(url)
 	if err != nil {
 		panic("Failed to connect to RabbitMQ: " + err.Error())
 	}
 
-	return conn
+	ch, err := conn.Channel()
+	if err != nil {
+		conn.Close()
+		panic("Fail to open channel: " + err.Error())
+	}
+
+	return &RabbitMQConnection{
+		Conn: conn,
+		Ch:   ch,
+	}
 }
